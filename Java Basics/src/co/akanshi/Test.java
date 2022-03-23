50bb76e7d0f19b3072fc29e8545bb646921d17fc
Akanshi Jain <akanshijain@gmail.com>
2022-03-23 17:43:33 +0530

    First Commit

---
+++ Java Basics/src/co/akanshi/Test.java
@@ -0,0 +1,18 @@
+package co.akanshi;
+
+public class Test{
+public static void main(String args [ ]){
+method1();
+method3();
+method2();
+}
+public static void method1(){
+System.out.println("Calling method 1");
+}
+public static void method2(){
+System.out.println("Calling method 3");
+}
+public static void method3(){
+System.out.println("Calling method 2");
+}
+}
